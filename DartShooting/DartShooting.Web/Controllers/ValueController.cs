9cb262712a236e507b16658d48e50710e95d2f9b
Xiangdong <zhangxd6@hotmail.com>
2015-10-28 22:05:09 -0500

    add stateless actor

--- DartShooting/DartShooting.Web/Controllers/ValueController.cs
+++ DartShooting/DartShooting.Web/Controllers/ValueController.cs
@@ -1,5 +1,8 @@
 ﻿using CountStateful.Contracts;
+using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Services;
+using StatelessActor.Interfaces;
+using StatelessActor.Interfaces.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,5 +29,16 @@ namespace DartShooting.Web.Controllers
             var proxy = ServiceProxy.Create<ICount>("999977808", new Uri("fabric:/DartShooting/CountStateful"));
             return proxy.GetCount().Result;
         } 
+
+
+        [HttpGet]
+        [Route("~/api/position")]
+        public Position GetPosition()
+        {
+            var actorId = ActorId.NewId();
+            var actorAddress = new Uri("fabric:/DartShooting/StatelessActorService");
+            IStatelessActor proxy = ActorProxy.Create<IStatelessActor>(actorId, actorAddress);
+            return proxy.GetRandomPosition().Result;
+        }
     }
 }
---
+++ DartShooting/StatelessActor/StatelessActor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using StatelessActor.Interfaces;
+using Microsoft.ServiceFabric;
+using Microsoft.ServiceFabric.Actors;
+using StatelessActor.Interfaces.Models;
+
+namespace StatelessActor
+{
+    public class StatelessActor : Actor, IStatelessActor
+    {
+
+        public async Task<Position> GetRandomPosition()
+        {
+            
+            ActorEventSource.Current.ActorMessage(this, "Generate Random position at partition "+ this.Host.StatelessServiceInitializationParameters.PartitionId +"and instance id " + Host.StatelessServiceInitializationParameters.InstanceId);
+            Random rnd = new Random(DateTime.UtcNow.Millisecond);
+            Position p = new Position()
+            {
+                X = rnd.Next(0, 400),
+                Y = rnd.Next(0, 800)
+            };
+            ActorEventSource.Current.ActorMessage(this, string.Format("new postion position ({0},{1})",p.X,p.Y));
+            return await Task.FromResult<Position>(p);
+
+        }
+    }
+}
